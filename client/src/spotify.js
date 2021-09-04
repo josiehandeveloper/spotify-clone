03e8c0fa907cb1adb8e99e6c04e98fc485c87a95
Josephine Han <jsphnhan@gmail.com>
2021-09-03 21:31:54 -0700

    login page made

---
+++ client/src/spotify.js
@@ -0,0 +1,33 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "0d0f1697c8ba4a3f8250b151292b6958";
+
+const scopes = [
+  "user-read-email",
+  "user-read-private",
+  "user-library-read",
+  "user-library-modify",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
